db25a9f5b8fc142b17277aa13dc6b6e56f996e93
LJohnson <latishaj966@gmail.com>
2020-09-01 16:07:14 -0700

    added a comment in source

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Navigation } from "./components";
 import configureStore from "./redux/configureStore";
-
+//Ask akil if this merge shows up
 const { store, persistor } = configureStore();
 
 const KWITTER = {
