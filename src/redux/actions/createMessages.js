94969d4b6e4f6c0e76eaa515c408a5b5431be109
LJohnson <latishaj966@gmail.com>
2020-09-02 14:02:47 -0700

    added my imports

---
+++ src/redux/actions/createMessages.js
@@ -0,0 +1,5 @@
+import api from "../../utils/api";
+
+export const CREATE_MESSAGE = "CREATE_MESSAGE";
+export const CREATE_MESSAGE_SUCCESS = "CREATE_MESSAGE_SUCCESS";
+export const CREATE_MESSAGE_FAILURE = "CREATE_MESSAGE_FAILURE";
