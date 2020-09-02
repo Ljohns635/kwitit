8f8ec11c8c5159c3e17cc23cdc12e1fa39f71565
LJohnson <latishaj966@gmail.com>
2020-09-02 10:54:39 -0700

    added my api import and added my const variables to actions

---
+++ src/redux/actions/getmessages.js
@@ -0,0 +1,5 @@
+import api from "../../utils/api";
+
+export const GET_MESSAGE = "GET_MESSAGE";
+export const GET_MESSAGE_SUCCESS = "GET_MESSAGE_SUCCESS";
+export const GET_MESSAGE_FAILURE = "GET_MESSAGE_FAILURE";
