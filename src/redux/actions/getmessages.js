b93c16218d63baf290b67e1d8d99b6f0d04bb007
LJohnson <latishaj966@gmail.com>
2020-09-02 19:24:19 -0700

    fixed things in my folders

--- src/redux/actions/getmessages.js
+++ src/redux/actions/getmessages.js
@@ -4,10 +4,10 @@ export const GET_MESSAGE = "GET_MESSAGE";
 export const GET_MESSAGE_SUCCESS = "GET_MESSAGE_SUCCESS";
 export const GET_MESSAGE_FAILURE = "GET_MESSAGE_FAILURE";
 
-export const getMessage = () => async (dispatch) => {
+export const getMessage = ({ messageId }) => async (dispatch) => {
   try {
     dispatch({ type: GET_MESSAGE });
-    const payload = await api.getMessages();
+    const payload = await api.getMessages({ messageId });
     dispatch({ type: GET_MESSAGE_SUCCESS, payload });
   } catch (err) {
     dispatch({ type: GET_MESSAGE_FAILURE, err });
