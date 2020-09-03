2ef4e0cde3641096d79c9faf07f3f2654b541b86
Martin Villa <martin.villa@yahoo.com>
2020-09-03 09:49:12 -0700

    changed file name and in index

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,25 +1,17 @@
 export * from "./auth";
 
-export * from "./messages";
+export * from "./messageList";
 
 export * from "./register";
-
 
 export * from "./getusers";
 
 export * from "./deleteusers";
 
-
 export * from "./getmessages";
 
-
 export * from "./createMessages";
-
 
 export * from "./deleteMessages";
 
 export * from "./likes";
-
-
-
-
