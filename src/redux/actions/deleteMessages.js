b111e7e8f6530957161f77ef85ef2f9610f60dda
LJohnson <latishaj966@gmail.com>
2020-09-02 17:36:22 -0700

    added the export const variables and imported api

---
+++ src/redux/actions/deleteMessages.js
@@ -0,0 +1,5 @@
+import api from "../../utils/api";
+
+export const DELETE_MESSAGE = "DELETE_MESSAGE";
+export const DELETE_MESSAGE_SUCCESS = "DELETE_MESSAGE_SUCCESS";
+export const DELETE_MESSAGE_FAILURE = "DELETE_MESSAGE_FAILURE";
