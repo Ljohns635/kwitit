fc17602521e41185a30b42eabd623f607863e60e
Martin Villa <martin.villa@yahoo.com>
2020-09-03 16:03:55 -0700

    fixed bugs

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -8,10 +8,9 @@ export * from "./getusers";
 
 export * from "./deleteusers";
 
-export * from "./getmessages";
-
 export * from "./createMessages";
 
 export * from "./deleteMessages";
 
 export * from "./likes";
+export * from "./getmessages";
--- src/redux/actions/messageList.js
+++ src/redux/actions/messageList.js
@@ -4,11 +4,13 @@ export const GETMESSAGELIST_START = "GETMESSAGELIST_START";
 export const GETMESSAGELIST_SUCCESS = "GETMESSAGELIST_SUCCESS";
 export const GETMESSAGELIST_FAILOR = "GETMESSAGELIST_FAILOR";
 
-export const getMessageList = () => async (dispatch, getstate) => {
+export const getMessageList = (messageId) => async (dispatch, getstate) => {
+  console.log(messageId);
   try {
     dispatch({ type: GETMESSAGELIST_START });
-    const payload = await api.getMessageList();
+    const payload = await api.getMessageList(messageId);
     dispatch({ type: GETMESSAGELIST_SUCCESS, payload });
+    console.log(payload);
   } catch (err) {
     dispatch({ type: GETMESSAGELIST_FAILOR, err });
   }
