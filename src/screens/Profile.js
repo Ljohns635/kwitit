8b5d30f5dfc338a64484c813851bb8565469f600
Latisha Johnson <latishaj966@gmail.com>
2020-09-04 18:46:54 +0000

    Update Profile.js

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { MenuContainer } from "../components";
-<<<<<<< HEAD
 import UserList from "../components/user-list/UserList"
-
-
-=======
 import { GetMessageList } from "../components";
->>>>>>> components
+
 export const ProfileScreen = () => (
   <>
     <MenuContainer />
     <h2>Profile</h2>
-<<<<<<< HEAD
+
     <UserList />
-=======
+
     <GetMessageList />
->>>>>>> components
+
   </>
 );
