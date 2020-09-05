21d9296caf986f9aca8008bbe4a75dedfa18755f
Martin Villa <martin.villa@yahoo.com>
2020-09-04 23:05:39 -0700

    cleared bug

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -14,18 +14,6 @@ export * from "./deleteMessages";
 
 export * from "./likes";
 
-<<<<<<< HEAD
-=======
-
-
-export * from "./getmessages";
-
 export * from "./userList";
 
-<<<<<<< HEAD
->>>>>>> c20b0635c954adf245947f9d92c9ae49fb139ed0
 export * from "./getmessages";
-=======
->>>>>>> 4876c1dd2ce60f88b59f6966cee32e21e5c6e565
-
-
