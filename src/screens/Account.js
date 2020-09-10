749fd90ad82b948c5defe5b579c79b04d0939fe4
LJohnson <latishaj966@gmail.com>
2020-09-10 08:39:33 -0700

    edited my account screen

---
+++ src/screens/Account.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { MenuContainer } from "../components";
+// import CreateMessages from "../components/create-messages/CreateMessages";
+import { Link } from "react-router-dom";
+// import { GetMessageList } from "../components";
+import DeleteUser from "../components/delete-user/DeleteUser";
+
+export const Account = () => (
+  <>
+    <MenuContainer />
+    <Link to="/">Go Home</Link>
+    <h2>Account Information</h2>
+    <DeleteUser />
+  </>
+);
