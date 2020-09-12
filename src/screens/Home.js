d74e9d8168f45011f5c45bc885d4e6c8e8c43c37
Akil Mac <akilmac@hotmail.com>
2020-09-11 18:22:49 -0700

    fixed width on page and added gif to 404

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 
 export const HomeScreen = () => (
   <>
-  <div id="container" style={{backgroundImage: "url('http://www.estavoyage.com/wp-content/uploads/2017/08/circuit-californie.jpg')", backgroundSize: "cover", height: "640px"}}>
+  <div id="container" style={{backgroundImage: "url('https://www.visitcalifornia.com/sites/default/files/styles/welcome_image/public/VC_BestPiers_Module_Hero_SantaMonica_Stock_RF_EHNWY3_1280x640.jpg')", backgroundSize: "cover", position: "fixed", minHeight: "100%", minWidth: "100%", backgroundPosition: "center"}}>
     <MenuContainer />
     <h2>Your favorite microblogging platform</h2>
     <LoginFormContainer />
